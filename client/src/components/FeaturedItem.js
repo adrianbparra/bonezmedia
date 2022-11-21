f14be6feaf37ddf58c0af143c7edafe085b62909
adrianbparra <adrianplane12@gmail.com>
2022-11-20 20:51:47 -0700

    Update FeaturedItem.js
    changed some content and added description

--- client/src/components/FeaturedItem.js
+++ client/src/components/FeaturedItem.js
@@ -3,18 +3,22 @@ import { Card, Image, Grid } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 
-function FeaturedItem() {
+function FeaturedItem(props) {
+    const {id,header,price,description} = props;
     return (
         <Grid.Column>
             <Card color='red' fluid as={Link} to="/shop/1k334j4563kl">
                 <Image src='https://react.semantic-ui.com/images/wireframe/image.png'/>
                 <Card.Content>
                     <Card.Header>
-                        Colorado
+                        Header
                     </Card.Header>
                     <Card.Meta>
                         $14.99
                     </Card.Meta>
+                    <Card.Description>
+                        Description
+                    </Card.Description>
                 </Card.Content>
             </Card>
         </Grid.Column>
