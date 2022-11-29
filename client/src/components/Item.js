d459e71b8b652952eb4c00265580da3c1bc46837
adrianbparra <adrianplane12@gmail.com>
2022-11-28 17:57:26 -0700

    Update Item.js
    Added props to item default to show data

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Grid, Icon, Item } from 'semantic-ui-react';
+import { Button, Icon, Item } from 'semantic-ui-react';
 
 import "./Item.css";
 
 
 function ItemDefault(props) {
+
+    const {id,name,description,meta,price,dateAdded} = props.item;
     return (
-        // <Grid.Column>
             <Item>
-                <Item.Image as={Link} to="/shop/id" src='https://react.semantic-ui.com/images/wireframe/image.png' />
+                <Item.Image as={Link} to={`/shop/${id}`} src='https://react.semantic-ui.com/images/wireframe/image.png' />
 
                 <Item.Content>
-                    <Item.Header as={Link} to="/shop/id">Header</Item.Header>
-                    <Item.Meta>$14.99</Item.Meta>
-                    <Item.Description>Description</Item.Description>
+                    <Item.Header as={Link} to={`/shop/${id}`}>{name}</Item.Header>
+                    <Item.Meta>${price.toFixed(2)}</Item.Meta>
+                    <Item.Description>{description}</Item.Description>
                     <Item.Extra>
-                    <Button className='add-to-cart-button' color='red' floated='right'>
-                        Add to Cart
-                        <Icon name='cart plus'/>
-                    </Button>
+
+                        <Button className='add-to-cart-button' color='red' floated='right'>
+                            Add to Cart
+                            <Icon name='cart plus'/>
+                        </Button>
+
                     </Item.Extra>
                 </Item.Content>
-
             </Item>
-        // </Grid.Column>
     );
 }
 
