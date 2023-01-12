06375ee97efd5df475355cbdf01e7ea6d7c4f696
adrianbparra <adrianplane12@gmail.com>
2023-01-11 18:35:09 -0700

    Added Home
    Added home details and using install item component.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Header,Grid, Button } from 'semantic-ui-react';
+import FeaturedItem from '../components/FeaturedItem.js';
+import InstallItem from '../components/InstallItem.js';
+
+import './Home.css';
 
 function Home(props) {
     return (
-        <div>
-            Home
-        </div>
+        <Container as={"main"} className='main'>
+
+            <Container textAlign='center' text>
+                <Header as="h1">Auto Audio & Media</Header>
+                <Header.Subheader>
+                    Everthing audio and media in automotive
+                </Header.Subheader>
+            </Container>
+
+            <Container className='home-shop' textAlign='center'>
+                <Header className='home-shop-header' as='h1'>Shop Bonez Media</Header>
+                
+                <Grid stackable className='home-shop-grid' >
+                    <Grid.Row columns={3} centered>
+                            <FeaturedItem/>
+                       
+                            <FeaturedItem/>
+                        
+                            <FeaturedItem/>
+                    </Grid.Row>
+                </Grid>
+
+                <Button color='red' size='massive' as={Link} to="/shop">Shop Now!</Button>
+            </Container>
+            <Container className='home-installs'>
+
+                <Header textAlign="center" as='h1'>Installs</Header>
+
+                <Grid stackable columns={2}>
+
+                    <InstallItem>
+                        This was an install on a Chevrolet Colorado 2007.
+                    </InstallItem>
+
+                </Grid>
+
+                <Container textAlign='center'>
+                    <Button color='red' size='massive' as={Link} to="/installs">More Installs!</Button>
+                </Container>
+            </Container>
+            
+        </Container>
     );
 }
 
