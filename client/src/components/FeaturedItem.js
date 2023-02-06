b6556652389bce457cd62cedbfbb8ed17170d2d7
adrianbparra <adrianplane12@gmail.com>
2023-02-05 19:48:57 -0700

    Update FeaturedItem.js
    applied prettier format

--- client/src/components/FeaturedItem.js
+++ client/src/components/FeaturedItem.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import { Card, Image, Grid } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Card, Image, Grid } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 function FeaturedItem(props) {
-    const {id,name,unit_amount,description,images} = props.product;
+    const { id, name, unit_amount, description, images } = props.product;
     return (
-        <Grid.Column >
-            <Card  color='red' fluid as={Link} to={`/shop/${id}`}>
-                {images.length > 0 && <Image src={images[0]}/>}
+        <Grid.Column>
+            <Card color="red" fluid as={Link} to={`/shop/${id}`}>
+                {images.length > 0 && <Image src={images[0]} />}
                 <Card.Content>
-                    <Card.Header>
-                        {name}
-                    </Card.Header>
-                    <Card.Meta>
-                        $ {(unit_amount/100).toFixed(2)}
-                    </Card.Meta>
-                    <Card.Description>
-                        {description}
-                    </Card.Description>
+                    <Card.Header>{name}</Card.Header>
+                    <Card.Meta>$ {(unit_amount / 100).toFixed(2)}</Card.Meta>
+                    <Card.Description>{description}</Card.Description>
                 </Card.Content>
             </Card>
         </Grid.Column>
